0b94f97a78cfef6b41a8792dee2d2b4249ccc011
Jake Turner <jakedanielturner@gmail.com>
2023-02-01 15:43:09 +0000

    Working API calls using request
    - Read in data from JSON file, and flattened for sending to translate API
    - Iterate through languages in .env file and call API with each language and value in JSON
    - Translated results are logged to console

--- src/file-service.ts
+++ src/file-service.ts
@@ -1,10 +1,22 @@
 import { readFile } from 'node:fs/promises';
 
 // Read in the TO TRANSLATE file
-export const readTranslationFile = async (): Promise<string> => {
+export const readTranslationFile = async (): Promise<object> => {
   try {
     const file = await readFile('to_translate/lang.json', 'utf8');
-    return file;
+    let fileObj = JSON.parse(file);
+    const formattedFileObj = {};
+
+    Object.keys(fileObj).forEach(key => {
+      const value = fileObj[key];
+      
+      if (typeof value === 'object') {
+        Object.assign(formattedFileObj, value)
+      } else {
+        formattedFileObj[key] = value
+      }
+    })
+    return formattedFileObj;
   } catch (err) {
     console.error(err);
     throw err;
--- src/translation-service.ts
+++ src/translation-service.ts
@@ -1,1 +1,63 @@
-// See https://github.com/MicrosoftTranslator/Text-Translation-API-V3-NodeJS/blob/master/Translate.js
+/* This simple app uses the '/translate' resource to translate text from
+one language to another. */
+
+import { resolve } from "path";
+import { send } from "process";
+
+/* This template relies on the request module, a simplified and user friendly
+way to make HTTP requests. */
+const request = require('request');
+const axios = require('axios').default;
+const uuidv4 = require('uuid/v4');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+var key_var = 'TRANSLATOR_TEXT_RESOURCE_KEY';
+if (!process.env[key_var]) {
+    throw new Error('Please set/export the following environment variable: ' + key_var);
+}
+var resourceKey = process.env[key_var];
+
+var endpoint_var = 'TRANSLATOR_TEXT_ENDPOINT';
+if (!process.env[endpoint_var]) {
+    throw new Error('Please set/export the following environment variable: ' + endpoint_var);
+}
+var endpoint = process.env[endpoint_var];
+
+var region_var = 'TRANSLATOR_TEXT_REGION';
+if (!process.env[region_var]) {
+    throw new Error('Please set/export the following environment variable: ' + region_var);
+}
+var region = process.env[region_var];
+
+/* If you encounter any issues with the base_url or path, make sure that you are
+using the latest endpoint: https://docs.microsoft.com/azure/cognitive-services/translator/reference/v3-0-translate */
+export function translateText(lang, value){
+    let options = {
+        method: 'POST',
+        baseUrl: endpoint,
+        url: 'translate',
+        qs: {
+          'api-version': '3.0',
+          'to': [lang]
+        },
+        headers: {
+          'Ocp-Apim-Subscription-Key': resourceKey,
+          'Ocp-Apim-Subscription-Region': region,
+          'Content-type': 'application/json',
+          'X-ClientTraceId': uuidv4().toString()
+        },
+        body: [{
+              'text': value
+        }],
+        json: true,
+    };
+    // console.log('lang: ', lang, 'value: ', value);
+    request(options, function(err, res, body){
+        console.log(JSON.stringify(body, null, 4));
+    });
+
+}  
+
+// Call the function to translate text.
