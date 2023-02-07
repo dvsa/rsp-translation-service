3a67069384cda943f789d003696b498f7fdfbf93
Jake Turner <jakedanielturner@gmail.com>
2023-02-07 17:58:05 +0000

    Development to 1758 070223
    - Updated main to break languages input into multiple arrays, which are then used to break up API calls to translation service to avoid hitting call limits
    - Began updating file service to handle appending missing fields to json files to create complete file
    - Modified fs.writeFile to write to json files in more structured way

--- src/file-service.ts
+++ src/file-service.ts
@@ -16,3 +16,24 @@ export const readTranslationFile = async (): Promise<object> => {
     throw err;
   }
 };
+
+function iterate(file) {
+  Object.keys(file).forEach((key) => {
+    if (typeof file[key] === 'object') {
+      file[key] = iterate(file[key]);
+    } else {
+      console.log(file[key]);
+    }
+  });
+  return file;
+}
+
+export const appendMissingFields = async (file) => {
+  try {
+    const baseFile = await readFile('to_translate/baseFile.json', 'utf-8');
+    console.log(iterate(baseFile));
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
--- src/translateFile.ts
+++ src/translateFile.ts
@@ -18,18 +18,14 @@ dotenv.config();
 }; */
 
 export const translateFile = async (lang: string, file: object) => {
-  // console.log('in', file);
   const promises = [];
   let counter = 0;
 
   function iterate(toTranslate) {
-    // console.log(x);
     Object.keys(toTranslate).forEach((key) => {
       if (typeof toTranslate[key] === 'object') {
         toTranslate[key] = iterate(toTranslate[key]);
       } else {
-        // console.log(lang, x[key]);
-        console.log(toTranslate[key]);
         toTranslate[key] = promises.push(translateText(lang, toTranslate[key]));
       }
     });
@@ -41,32 +37,18 @@ export const translateFile = async (lang: string, file: object) => {
       if (typeof x[key] === 'object') {
         x[key] = assignPromises(x[key], translatedValues);
       } else {
-        // console.log(x[key]);
         x[key] = translatedValues[counter];
         counter++;
       }
     });
     return x;
-  };
+  }
 
   async function complete(fileToMatch) {
     const res = await iterate(fileToMatch);
     const moreRes = await Promise.all(promises).then((resolvedPromises) => assignPromises(res, resolvedPromises));
-    // console.log(promises);
     return moreRes;
-  };
-
-  /* async function complete(lang: string, file: object) {
-    // const moreRes = await Promise.all(promises).then((translatedValues) => assignPromises(res, translatedValues));
-    const moreRes = await Promise.all(
-      Object.keys(file).map(async (key) => {
-        const someRes = await translateText(lang, file[key]);
-        console.log(someR)
-      }),
-    ).then((value) => console.log('then: ', value));
-    console.log('translateFile.ts: ', moreRes);
-    return moreRes;
-  }; */
+  }
 
   const finale = await complete(file);
   return finale;
