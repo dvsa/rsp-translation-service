bf441ef599cbbec5820b31956507e8a8b52c69b8
Anna Dodson <Anna.Dodson@DVSA.gov.uk>
2023-02-07 22:02:27 +0000

    Couldn't let this go
    - Reworked the iterate function to work synchronously and ignore all the
      TypeScript warnings
    - Update to tests
    - TODO - test with microsoft api

--- src/translateFile.ts
+++ src/translateFile.ts
@@ -1,55 +1,21 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable guard-for-in */
-/* eslint-disable no-prototype-builtins */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable security/detect-object-injection */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable no-await-in-loop */
 /* eslint-disable no-restricted-syntax */
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
-import * as dotenv from 'dotenv';
 import { translateText } from './translation-service';
 
-dotenv.config();
-
-/* export const translateFile = async <Type> (lang: string, file: object): Promise<Type> => {
-  return translation as Type;
-}; */
-
-export const translateFile = async (lang: string, file: object) => {
-  const promises = [];
-  let counter = 0;
-
-  function iterate(toTranslate) {
-    Object.keys(toTranslate).forEach((key) => {
+export const translateFile = async <Type> (lang: string, file: Type) : Promise<Type> => {
+  async function iterate(toTranslate: Type) {
+    const translatedFile = {};
+    for (const key of Object.keys(toTranslate)) {
       if (typeof toTranslate[key] === 'object') {
-        toTranslate[key] = iterate(toTranslate[key]);
+        translatedFile[key] = await iterate(toTranslate[key] as Type);
       } else {
-        toTranslate[key] = promises.push(translateText(lang, toTranslate[key]));
+        translatedFile[key] = await translateText(lang, toTranslate[key]);
       }
-    });
-    return toTranslate;
+    }
+    return translatedFile;
   }
 
-  function assignPromises(x, translatedValues) {
-    Object.keys(x).forEach((key) => {
-      if (typeof x[key] === 'object') {
-        x[key] = assignPromises(x[key], translatedValues);
-      } else {
-        x[key] = translatedValues[counter];
-        counter++;
-      }
-    });
-    return x;
-  }
-
-  async function complete(fileToMatch) {
-    const res = await iterate(fileToMatch);
-    const moreRes = await Promise.all(promises).then((resolvedPromises) => assignPromises(res, resolvedPromises));
-    return moreRes;
-  }
-
-  const finale = await complete(file);
-  return finale;
+  const translatedJson = await iterate(file);
+  return translatedJson as Type;
 };
--- tests/translateFile.test.ts
+++ tests/translateFile.test.ts
@@ -1,25 +1,57 @@
 import { translateFile } from '../src/translateFile';
-// import { translateText } from '../src/translation-service';
 
-describe.only('translateFile', () => {
-  const mockTranslateText = jest.fn();
-  const mockTranslateFile = jest.mock('../src/translation-service', () => ({
-    translateText: mockTranslateText,
-  }));
+const mockTranslateText = jest.fn();
+
+jest.mock('../src/translation-service', () => ({
+  translateText: () => mockTranslateText() as Promise<string>,
+}));
+
+const language = 'fr';
+
+const title = 'Hello, World!';
+
+const translationTitle = 'Salut tout le monde!';
+const translationNested = 'je suis un objet imbriqué';
+const translationLink = 'Cliquez ici';
+
+describe('translateFile', () => {
+  afterEach(() => {
+    mockTranslateText.mockRestore();
+  });
+
   test('Translates a given object', async () => {
-    mockTranslateText.mockResolvedValue('Salut tout le monde!');
-    const result = await translateFile('fr', { title: 'Hello, World!' });
-    expect(result.title).toBe('Salut tout le monde!');
+    mockTranslateText.mockResolvedValue(translationTitle);
+    const result = await translateFile(language, { title });
+    expect(result.title).toBe(translationTitle);
   });
 
   test('Translates a nested object', async () => {
-    mockTranslateText.mockResolvedValue('Salut tout le monde!');
-    const result = await translateFile('fr', { header: { title: 'Hello, World!' } });
-    expect(result.header.title).toBe('Salut tout le monde!');
+    mockTranslateText.mockResolvedValue(translationTitle);
+    const result = await translateFile(language, { header: { title } });
+    expect(result.header.title).toBe(translationTitle);
   });
 
   test('Translate a nested nested object', async () => {
-    const result = await translateFile('fr', { header: { title: 'Hello, World!', subtitle: { paragraph: 'I am a nested object' } } });
-    expect(result.header.subtitle.paragraph).toBe('Je suis un objet imbriqué');
+    mockTranslateText.mockResolvedValueOnce(translationTitle);
+    mockTranslateText.mockResolvedValueOnce(translationNested);
+    mockTranslateText.mockResolvedValueOnce(translationLink);
+    const result = await translateFile(
+      language,
+      {
+        header:
+        {
+          title: 'Hello, World!',
+          subtitle: { paragraph: 'I am a nested object' },
+        },
+        link: 'click here',
+      },
+    );
+    expect(result).toEqual({
+      header: {
+        title: translationTitle,
+        subtitle: { paragraph: translationNested },
+      },
+      link: translationLink,
+    });
   });
 });
